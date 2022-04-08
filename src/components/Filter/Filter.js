0014e40371e0c559df87bb728d7c066088386a61
Cozy <devcosmas@gmail.com>
2022-04-08 13:50:19 +0300

    UI improvements

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -7,10 +7,22 @@ import './Filter.scss';
 import sprite from '../../assets/icons/sprite.svg';
 
 function Filter() {
-	const options = [
-		{ value: 'chocolate', label: 'Chocolate' },
-		{ value: 'strawberry', label: 'Strawberry' },
-		{ value: 'vanilla', label: 'Vanilla' },
+	const categoryOptions = [
+		{ value: 'chocolate', label: 'Chicken' },
+		{ value: 'strawberry', label: 'Beef' },
+		{ value: 'vanilla', label: 'Dessert' },
+	];
+
+	const areaOptions = [
+		{ value: 'chocolate', label: 'Kenya' },
+		{ value: 'strawberry', label: 'Uganda' },
+		{ value: 'vanilla', label: 'Tanzania' },
+	];
+
+	const ingredientsOptions = [
+		{ value: 'chocolate', label: 'Rosemary' },
+		{ value: 'strawberry', label: 'Strawberries' },
+		{ value: 'vanilla', label: 'Ghee' },
 	];
 
 	const [filterOpen, setFilterOpen] = useState(false);
@@ -27,21 +39,21 @@ function Filter() {
 					<label htmlFor=''>Category</label>
 					<input type='radio' name='filter' id='filter' />
 				</div>
-				<Select options={options} />
+				<Select options={categoryOptions} />
 			</div>
 			<div className='filter__action--group'>
 				<div className='filter__action--wrapper u-margin-bottom-small'>
 					<label htmlFor=''>Area</label>
 					<input type='radio' name='filter' id='filter' />
 				</div>
-				<Select options={options} />
+				<Select options={areaOptions} />
 			</div>{' '}
 			<div className='filter__action--group'>
 				<div className='filter__action--wrapper u-margin-bottom-small'>
 					<label htmlFor=''>Ingredient</label>
 					<input type='radio' name='filter' id='filter' />
 				</div>
-				<Select options={options} />
+				<Select options={ingredientsOptions} />
 			</div>
 		</div>
 	);
@@ -49,7 +61,7 @@ function Filter() {
 	return (
 		<div className='filter'>
 			<nav className='filter__nav'>
-				<h4 className='heading--sub'>The Information Title</h4>
+				<h4 className='heading--sub'>Advanced Selection</h4>
 				<ul className='filter__nav--list'>
 					{/* <li className='filter__nav--item'>
 						<Link
